3ca266b202fac7a3bca0799f8681ef67f5493750
Jeiojey <signofaries@gmail.com>
2023-03-12 19:58:44 +0300

    feat: add log

--- Java_test/ToysList.java
+++ Java_test/ToysList.java
@@ -1,3 +1,4 @@
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -6,6 +7,7 @@ import java.util.Random;
 public class ToysList<T extends Toy> implements Iterable<Toy> {
 
     List<Toy> toysList = new ArrayList<>();
+    private String file = "winnings.txt";
 
     private void checkList() {
         for (int i = 0; i < toysList.size(); i++) {
@@ -38,11 +40,27 @@ public class ToysList<T extends Toy> implements Iterable<Toy> {
         for (int i = 0; i < toysList.size(); i++) {
             randomChance -= Integer.parseInt(toysList.get(i).getWeight());
             if (randomChance <= 0) {
-                System.out.printf("Вы выиграли %s!\n", toysList.get(i).getName());
+
+                System.out.printf("You won %s!\n", toysList.get(i).getName());
                 int amount = Integer.parseInt(toysList.get(i).getAmount()) - 1;
+                log(i);
                 toysList.get(i).setAmount(Integer.toString(amount));
                 break;
             }
+        }
+    }
+
+    private void log(int i) {
+        try (FileWriter writer = new FileWriter(file, true)) {
+            writer.write("User won ");
+            writer.append(toysList.get(i).getName());
+            writer.append(". ");
+            writer.write("Toys left ");
+            writer.append(toysList.get(i).getAmount());
+            writer.append(".\n");
+            writer.flush();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
